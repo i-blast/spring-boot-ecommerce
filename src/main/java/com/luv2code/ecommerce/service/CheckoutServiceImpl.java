ab255913c5b49e528edaec3f34615dda596a0a6e
ilYa <i.blast59@gmail.com>
2022-03-24 03:11:21 +0300

    feat(checkout): Add order saving to database

---
+++ src/main/java/com/luv2code/ecommerce/service/CheckoutServiceImpl.java
@@ -0,0 +1,48 @@
+package com.luv2code.ecommerce.service;
+
+import com.luv2code.ecommerce.dao.CustomerRepository;
+import com.luv2code.ecommerce.dto.Purchase;
+import com.luv2code.ecommerce.dto.PurchaseResponse;
+import com.luv2code.ecommerce.entity.Customer;
+import com.luv2code.ecommerce.entity.Order;
+import com.luv2code.ecommerce.entity.OrderItem;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+import java.util.Set;
+import java.util.UUID;
+
+@Service
+public class CheckoutServiceImpl implements CheckoutService {
+
+    private CustomerRepository customerRepository;
+
+    public CheckoutServiceImpl(CustomerRepository customerRepository) {
+        this.customerRepository = customerRepository;
+    }
+
+    @Transactional
+    @Override
+    public PurchaseResponse placeOrder(Purchase purchase) {
+        Order order = purchase.getOrder();
+
+        String orderTrackingNumber = generateOrderTrackingNumber();
+        order.setOrderTrackingNumber(orderTrackingNumber);
+
+        Set<OrderItem> orderItems = purchase.getOrderItems();
+        orderItems.forEach(item -> order.add(item));
+
+        order.setShippingAddress(purchase.getShippingAddress());
+        order.setBillingAddress(purchase.getBillingAddress());
+
+        Customer customer = purchase.getCustomer();
+        customer.add(order);
+        customerRepository.save(customer);
+
+        return new PurchaseResponse(orderTrackingNumber);
+    }
+
+    private String generateOrderTrackingNumber() {
+        return UUID.randomUUID().toString();
+    }
+}
