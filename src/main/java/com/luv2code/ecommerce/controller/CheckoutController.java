28cce13bdbf4a48ee2576ea71f095f01dddf7019
ilYa <i.blast59@gmail.com>
2022-04-04 16:26:46 +0300

    fix(payment): Round payment amount value before passing to the backend

--- src/main/java/com/luv2code/ecommerce/controller/CheckoutController.java
+++ src/main/java/com/luv2code/ecommerce/controller/CheckoutController.java
@@ -6,13 +6,20 @@ import com.luv2code.ecommerce.dto.PurchaseResponse;
 import com.luv2code.ecommerce.service.CheckoutService;
 import com.stripe.exception.StripeException;
 import com.stripe.model.PaymentIntent;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.*;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 @RequestMapping("/api/checkout")
 public class CheckoutController {
+
+    private static final Logger logger = LoggerFactory.getLogger(CheckoutController.class.getName());
 
     private CheckoutService checkoutService;
 
@@ -28,6 +35,7 @@ public class CheckoutController {
 
     @PostMapping("/payment-intent")
     public ResponseEntity<String> createPaymentIntent(@RequestBody PaymentInfo paymentInfo) throws StripeException {
+        logger.info("paymentInfo.amount: " + paymentInfo.getAmount());
         PaymentIntent paymentIntent = checkoutService.createPaymentIntent(paymentInfo);
         String paymentStr = paymentIntent.toJson();
         return new ResponseEntity<>(paymentStr, HttpStatus.OK);
